1e94cdad54ae4cae4a019d27ba15a65c740c0db6
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-09-23 23:07:46 +0100

    getters and setters

--- index.php
+++ index.php
@@ -22,23 +22,46 @@
         public function signOut() {
             return "Signed out user name is $this->name and email is $this->email";
         }
+
+
+        // getter get and return a private property for us
+        public function getName(){
+            return $this->name;
+        }
+
+
+        // setter sets or change a private property for us
+        public function setEmail($newEmail){
+            if(strpos($newEmail, '@')){
+                $this->email = $newEmail;
+            }
+        }
+
     }
 
 
-    
-
     // Creating an object (instantiating the class)
-    $userOne = new User('Mario', 'mario@uk.co <br />', 19);
-    $userTwo = new User('Yoshi', 'Yoshi@netninja.uk <br />', 24);
+    $userOne = new User('Mario', 'mario@uk.co', 19);
+    $userTwo = new User('Yoshi', 'Yoshi@netninja.uk', 24);
     $userThree = new User('Ryu', 'ryu@ninja.co', 27);
 
     // Calling methods and accessing public properties
-    echo $userOne->signOut();  
-    echo $userTwo->login();    
-    echo $userThree->age;      
+    echo $userOne->signOut() . '<br />';  
+    echo $userTwo->login() . '<br />';    
+    echo $userThree->age . '<br />';      
 
-    // The following line will not work because $name is private
-    // echo $userThree->name; 
+    ////// The following line will not work because $name is private
+    //echo $userThree->name; 
+
+    // now we can echo/access a private property with the getter nethod
+    echo $userThree->getName() . '<br />'; 
+
+    // now we can echo/access/modify the email using the setter method.
+    echo $userThree->setEmail('ryu@gmail.com') . '<br />';
+
+    // echo to test the chnaged email
+    echo $userThree->login() . '<br />';
+
 
 ?>
 
